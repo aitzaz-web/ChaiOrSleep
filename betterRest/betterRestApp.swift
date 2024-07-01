b66117b5d649708dc10a2db4164aaf335598644f
Aitzaz Munir Shaikh <ams845@cornell.edugit config --global user.email ams845@cornell.edu>
2024-07-01 19:07:58 +0500

    Initial Commit

---
+++ betterRest/betterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterRestApp.swift
+//  betterRest
+//
+//  Created by Aitzaz Munir on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct betterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
